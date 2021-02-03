ae6a95f75f078a44d3a0446d107d9386bf6794f1
Will Becker <william.becker@bluefoxdata.eu>
2021-02-03 16:52:27 +0100

    Added some common denominator data

--- R/data.R
+++ R/data.R
@@ -28,3 +28,13 @@
 #'
 #' @source \url{https://composite-indicators.jrc.ec.europa.eu/asem-sustainable-connectivity/repository}
 "ASEMAggMeta"
+
+#' World denomination data
+#'
+#' A small selection of common denominator indicators, which includes GDP, Population, Area, GDP per capita
+#' and income group. All data sourced from the World Bank as of Feb 2020 (data is typically from 2019).
+#'
+#' @format A data frame with 249 rows and 7 variables.
+#'
+#' @source \url{https://data.worldbank.org/}
+"WorldDenoms"
