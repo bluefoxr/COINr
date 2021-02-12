64b3c1672b5369b254753605a2ff074986939528
Will Becker <william.becker@bluefoxdata.eu>
2021-02-12 09:07:16 +0100

    Enabled plotly histogram in same function as violin. Renamed function.

--- R/coin_iplot.R
+++ R/coin_iplot.R
@@ -1,12 +1,13 @@
-#' Interactive violin plot
+#' Interactive indicator distribution plots
 #'
-#' Generates a javascript violin plot of a single indicator, using Plotly. Plot can be embedded e.g. in
+#' Generates a javascript distribution plot of a single indicator, using Plotly. Plot can be embedded e.g. in
 #' HTML documents, websites, etc, or used for more interactive data exploration. This only plots one
 #' indicator at a time - for multiple plots you can use plotIndDist()
 #'
 #' @param COINobj The COIN object, or a data frame of indicator data
 #' @param dset The source data set to use for indicator data (if input is COIN object)
 #' @param inames A character vector of a single indicator name or aggregate name to plot.
+#' @param ptype The type of plot to produce. Currently supports "Violin" and "Histogram".
 #' @param aglev The aggregation level to extract the indicator data from. Defaults to indicator level (1)
 #'
 #' @importFrom reshape2 melt
@@ -18,16 +19,17 @@
 #'
 #' @export
 
-iplotViolin <- function(COINobj, dset = "Raw", inames = NULL, aglev = 1, axlims = NULL){
+iplotIndDist <- function(COINobj, dset = "Raw", inames = NULL, ptype = "Violin", aglev = 1,
+                        axlims = NULL){
 
   out1 <- getIn(COINobj, dset = dset, inames = inames, aglev = aglev)
   ind_data_only <- out1$ind_data_only
   ind_names <- out1$IndNames
   ind_code <- out1$IndCodes
 
-  if(length(ind_names)>1){stop("This function only supports plotting single indicators.")}
+  if(length(ind_names)>1){stop("This function only supports plotting single indicators. You may need to use the aglev argument if you are calling an aggregation group.")}
 
-  if(out1$otype=="COINobj"){
+  if((out1$otype=="COINobj") & (aglev == 1)){
     # look for units
     if(exists("IndUnit",COINobj$Input$IndMeta)){
       # find unit for indicator
@@ -41,24 +43,43 @@ iplotViolin <- function(COINobj, dset = "Raw", inames = NULL, aglev = 1, axlims 
     indunit <- ""
   }
 
-  fig <- plotly::plot_ly(data =ind_data_only, y = ~get(ind_code), type = 'violin',
-                         box = list(visible = T),
-                         meanline = list(visible = T),
-                         x0 = ind_names,
-                         points = 'all',
-                         pointpos = -1.5,
-                         jitter = 0.1,
-                         hoveron = "points+kde",
-                         color = I("#8dd3c7"),
-                         marker = list(
-                           line = list(
-                             width = 2,
-                             color = "#8dd3c7"
-                           ),
-                           symbol = 'line-ew'
-                         )
-  )
-  fig <- fig %>% plotly::layout( yaxis = list(title = indunit, zeroline = F, range = axlims) )
+  if (ptype == "Violin"){
+
+    fig <- plotly::plot_ly(data =ind_data_only, y = ~get(ind_code), type = 'violin',
+                           box = list(visible = T),
+                           meanline = list(visible = T),
+                           x0 = ind_names,
+                           points = 'all',
+                           pointpos = -1.5,
+                           jitter = 0.1,
+                           hoveron = "points+kde",
+                           color = I("#8dd3c7"),
+                           marker = list(
+                             line = list(
+                               width = 2,
+                               color = "#8dd3c7"
+                             ),
+                             symbol = 'line-ew'
+                           )
+    )
+
+    # scale axes (for matching with another plot, for example).
+    # Not scaled if axlims not specified (range = NULL does nothing)
+    fig <- fig %>% plotly::layout( yaxis = list(title = indunit, zeroline = F, range = axlims) )
+
+  } else if (ptype == "Histogram"){
+
+    fig <- plot_ly(data = ind_data_only, x = ~get(ind_code), type = "histogram")
+
+    # scale axes (for matching with another plot, for example).
+    # Not scaled if axlims not specified (range = NULL does nothing)
+    fig <- fig %>% plotly::layout(bargap=0.1, xaxis = list(title = indunit, range = axlims),
+                                  yaxis = list(title = "Count"),
+                                  title = ind_names)
+
+    fig
+
+  }
 
   fig
 
