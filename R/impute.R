b2afe628903e4c2de95afab69e2d02b5bc1d30e5
Will Becker <william.becker@bluefoxdata.eu>
2022-03-29 11:05:32 +0200

    New impute methods

--- R/impute.R
+++ R/impute.R
@@ -1,16 +1,57 @@
 # IMPUTATION
 
+#' Impute indicators
+#'
+#' @param x A purse object
+#' @param dset The data set to impute in each coin
+#' @param f_i xx
+#' @param f_i_para xx
+#' @param impute_by xx
+#' @param group_level xx
+#' @param normalise_first xx
+#' @param write_to xx
+#'
+#' @return An updated purse with imputed data sets added to each coin.
+#' @export
+#'
+#' @examples
+#' #
+impute2.purse <- function(x, dset = NULL, f_i = NULL, f_i_para = NULL, impute_by = "column",
+                         group_level = NULL, normalise_first = NULL, write_to = NULL){
+
+  # input check
+  check_purse(x)
+
+  # apply unit screening to each coin
+  x$coin <- lapply(x$coin, function(coin){
+    impute2.coin(coin, dset = dset, f_i = f_i, f_i_para = f_i_para, impute_by = impute_by,
+                 group_level = group_level, normalise_first = normalise_first, out2 = "coin",
+                 write_to = write_to)
+  })
+
+  # make sure still purse class
+  class(x) <- c("purse", "data.frame")
+  x
+}
+
 
 #' Impute a data set in a coin
 #'
+#' If `normalise_first = TRUE` (default for `impute_by = "row"`), indicators will be
+#' normalised first using the min-max method, and returned to their original scales after
+#' imputation.
+#'
 #' @param x A coin class object
 #' @param dset A data set within the coin
-#' @param f_i An imputation function
-#' @param f_i_para Further arguments to pass to `f_i`
-#' @param impute_by s
-#' @param group_level xx
-#' @param norm_specs xx
-#' @param ... further arguments to be passed to [normalise()]
+#' @param f_i An imputation function. See details.
+#' @param f_i_para Further arguments to pass to `f_i`, other than `x`. See details.
+#' @param impute_by xx
+#' @param group_level A level of the framework to use for grouping indicators. This is only
+#' relevant if `impute_by = "row"`. In that case, indicators will be split into their groups at the
+#' level specified by `group_level`, and imputation will be performed across rows of the group, rather
+#' than the whole data set. This can make more sense because indicators within a group are likely to be
+#' more similar.
+#' @param normalise_first xx
 #' @param out2 Either `"coin"` to return normalised data set back to the coin, or `df` to simply return a data
 #' frame.
 #' @param write_to Optional character string for naming the data set in the coin. Data will be written to
@@ -22,7 +63,7 @@
 #' @examples
 #' #
 impute2.coin <- function(x, dset = NULL, f_i = NULL, f_i_para = NULL, impute_by = "column",
-                         group_level = NULL, norm_specs = NULL, ..., out2 = "coin",
+                         group_level = NULL, normalise_first = NULL, out2 = "coin",
                          write_to = NULL){
 
   # WRITE LOG ---------------------------------------------------------------
@@ -34,20 +75,32 @@ impute2.coin <- function(x, dset = NULL, f_i = NULL, f_i_para = NULL, impute_by 
   iData <- get_dset(coin, dset)
   iData_ <- iData[colnames(iData) != "uCode"]
 
+  # if normalise_first not specified set TRUE if rowwise or FALSE for anything else
+  if(is.null(normalise_first)){
+    if(impute_by == "row"){
+      normalise_first <- TRUE
+    } else {
+      normalise_first <- FALSE
+    }
+  }
 
-  # NORMALISE ---------------------------------------------------------------
-
-  # to finish
-
+  # get directions if needed: these are the directions in iMeta
+  if(!is.null(normalise_first)){
+    directions <- coin$Meta$Ind[c("iCode", "Direction")]
+  }
 
   # IMPUTE DATA ----------------------------------------------------------
 
   # first, we may need to split
   if(is.null(group_level)){
 
+    directions <- directions$Direction[match(colnames(iData_), directions$iCode)]
+
     # no splitting here = easy
     iData_i <- impute2.data.frame(iData_, f_i = f_i, f_i_para = f_i_para,
-                                  impute_by = impute_by)
+                                  impute_by = impute_by,
+                                  normalise_first = normalise_first,
+                                  directions = directions)
 
   } else {
 
@@ -67,8 +120,10 @@ impute2.coin <- function(x, dset = NULL, f_i = NULL, f_i_para = NULL, impute_by 
 
     # now do the imputation
     iData_split_i <- lapply(iData_split, function(dfi){
+      directions <- directions$Direction[match(colnames(dfi), directions$iCode)]
       impute2.data.frame(dfi, f_i = f_i, f_i_para = f_i_para,
-                         impute_by = impute_by)
+                         impute_by = impute_by, normalise_first = normalise_first,
+                         directions = directions)
     })
 
     # reassemble
@@ -84,6 +139,9 @@ impute2.coin <- function(x, dset = NULL, f_i = NULL, f_i_para = NULL, impute_by 
     }
 
   }
+
+
+  # OUTPUT ------------------------------------------------------------------
 
   # reunite with uCode col
   iData_i <- cbind(uCode = iData$uCode, iData_i)
@@ -113,8 +171,26 @@ impute2.coin <- function(x, dset = NULL, f_i = NULL, f_i_para = NULL, impute_by 
 #'
 #' Clearly, the function `f_i` needs to be able to accept with the data class passed to it - if
 #' `impute_by` is `"row"` or `"column"` this will be a numeric vector, or if `"df"` it will be a data
-#' frame. Checks are made on the format of the data returned by imputation functions, to ensure the
-#' type and that non-`NA` values have not been inadvertently altered.
+#' frame.
+#'
+#' When imputing row-wise, prior normalisation of the data is recommended. This is because imputation
+#' will use e.g. the mean of the unit values over all indicators (columns). If the indicators are on
+#' very different scales, the result will likely make no sense. If the indicators are normalised first,
+#' more sensible results can be obtained. There are two options to pre-normalise: first is by setting
+#' `normalise_first = TRUE` - this is anyway the default if `impute_by = "row"`. In this case, you also
+#' need to supply a vector of directions. The data will then be normalised using a min-max approach
+#' before imputation, followed by the inverse operation to return the data to the original scales.
+#'
+#' Another approach which gives more control is to simply run [normalise()] first, and work with the
+#' normalised data from that point onwards. In that case it is better to set `normalise_first = FALSE`,
+#' since by default if `impute_by = "row"` it will be set to `TRUE`.
+#'
+#' Checks are made on the format of the data returned by imputation functions, to ensure the
+#' type and that non-`NA` values have not been inadvertently altered. This latter check is allowed
+#' a degree of tolerance for numerical precision, controlled by the `sfigs` argument. This is because
+#' if the data frame is normalised, and/or depending on the imputation function, there may be a very
+#' small differences. By default `sfigs = 9`, meaning that the non-`NA` values pre and post-imputation
+#' are compared to 9 significant figures.
 #'
 #' @param x A data frame with only numeric columns.
 #' @param f_i A function to use for imputation. By default, imputation is performed by simply substituting
@@ -123,13 +199,20 @@ impute2.coin <- function(x, dset = NULL, f_i = NULL, f_i_para = NULL, impute_by 
 #' @param impute_by Specifies how to impute: if `"column"`, passes each column separately as a numerical
 #' vector to `f_i`; if `"row"`, passes each *row* separately; and if `"df"` passes the entire data frame to
 #' `f_i`. The function called by `f_i` should be compatible with the type of data passed to it.
+#' @param normalise_first Logical: if `TRUE`, each column is normalised using a min-max operation before
+#' imputation. By default this is `FALSE` unless `impute_by = "row"`. See details.
+#' @param directions A vector of directions: either -1 or 1 to indicate the direction of each column
+#' of `x` - this is only used if `normalise_first = TRUE`. See details.
+#' @param sfigs The number of significant figures to use in comparing non-`NA` values in `x` with their
+#' counterparts in the imputed data frame. Default 9. See details.
 #'
 #' @return
 #' @export
 #'
 #' @examples
 #' #
-impute2.data.frame <- function(x, f_i = NULL, f_i_para = NULL, impute_by = "column"){
+impute2.data.frame <- function(x, f_i = NULL, f_i_para = NULL, impute_by = "column",
+                               normalise_first = NULL, directions = NULL, sfigs = 9){
 
   # CHECKS ------------------------------------------------------------------
 
@@ -150,12 +233,48 @@ impute2.data.frame <- function(x, f_i = NULL, f_i_para = NULL, impute_by = "colu
     return(x)
   }
 
+  # NORMALISE ---------------------------------------------------------------
+
+  # if normalise_first not specified set TRUE if rowwise or FALSE for anything else
+  if(is.null(normalise_first)){
+    if(impute_by == "row"){
+      normalise_first <- TRUE
+    } else {
+      normalise_first <- FALSE
+    }
+  }
+
+  if(normalise_first){
+
+    if(is.null(directions)){
+      stop("To normalise the data you need to specify the 'directions' argument. If you are normalising
+           by row, normalisation is recommended and is switched on by default.")
+    }
+
+    # checks
+    stopifnot(length(directions) == ncol(x),
+              all(directions %in% c(-1, 1)))
+
+    # adjust for directions
+    x_n <- as.data.frame(mapply(`*`, x, directions))
+
+    # we will need the original min and max to reconstruct
+    xmins <- sapply(x_n, min, na.rm = T)
+    xmaxs <- sapply(x_n, max, na.rm = T)
+
+    x_n <- as.data.frame(lapply(x_n, function(x){
+      (x - min(x, na.rm = TRUE))/(max(x, na.rm = TRUE) - min(x, na.rm = TRUE))
+    }))
+  } else {
+    x_n <- x
+  }
+
   # IMPUTE ------------------------------------------------------------------
 
   if(impute_by == "df"){
 
     # function args
-    f_args <- list(x = x)
+    f_args <- list(x = x_n)
     if(!is.null(f_i_para)){
       if(!is.list(f_i_para)){
         stop("f_i_para must be a list")
@@ -176,18 +295,18 @@ impute2.data.frame <- function(x, f_i = NULL, f_i_para = NULL, impute_by = "colu
   } else if (impute_by == "column") {
 
     if(is.null(f_i_para)){
-      x_imp <- lapply(x, impute2.numeric, f_i)
+      x_imp <- lapply(x_n, impute2.numeric, f_i)
     } else {
-      x_imp <- lapply(x, impute2.numeric, f_i, f_i_para)
+      x_imp <- lapply(x_n, impute2.numeric, f_i, f_i_para)
     }
 
   } else if (impute_by == "row"){
 
     # work row-wise with apply
     if(is.null(f_i_para)){
-      x_imp <- apply(x, 1, impute2.numeric, f_i, simplify = FALSE)
+      x_imp <- apply(x_n, 1, impute2.numeric, f_i, simplify = FALSE)
     } else {
-      x_imp <- apply(x, 1, impute2.numeric, f_i, f_i_para, simplify = FALSE)
+      x_imp <- apply(x_n, 1, impute2.numeric, f_i, f_i_para, simplify = FALSE)
     }
 
     # I have to reassemble carefully into a df
@@ -198,7 +317,22 @@ impute2.data.frame <- function(x, f_i = NULL, f_i_para = NULL, impute_by = "colu
   x_imp <- as.data.frame(x_imp)
 
 
-  # OUTPUT ------------------------------------------------------------------
+  # UN-NORMALISE ------------------------------------------------------------
+
+  if(normalise_first){
+
+    # do normalisation backwards
+    x_imp <- mapply(function(x, mn, mx, direc){
+      xo <- x*(mx - mn) + mn
+      xo*direc
+    }, x_imp, xmins, xmaxs, directions)
+
+    x_imp <- as.data.frame(x_imp)
+
+  }
+
+
+  # CHECKS AND OUTPUT -------------------------------------------------------------
 
   # manually reset row names
   row.names(x_imp) <- attr(x, "row.names")
@@ -207,9 +341,18 @@ impute2.data.frame <- function(x, f_i = NULL, f_i_para = NULL, impute_by = "colu
   x_check <- x_imp
   x_check[x_NAs] <- NA
 
-  if(!identical(x_check, x)){
-    warning("Differences detected in non-NA values of imputed data frame.")
+  # to cross check we also have to make all cols from x numeric: some may be integer
+  # columns that are changed to numeric during imputation. In this case, identical()
+  # will return false even if the values are the same
+  x <- as.data.frame(lapply(x, as.numeric))
+  row.names(x) <- attr(x_imp, "row.names")
+
+  if(!identical(signif(x_check, sfigs), signif(x, sfigs))){
+    stop("Differences detected in non-NA values of imputed data frame.")
   }
+
+  # replace non-NA values with original values to avoid any numerical precision issues
+  x_imp[!x_NAs] <- x[!x_NAs]
 
   x_imp
 
