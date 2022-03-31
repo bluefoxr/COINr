e8f92ae8488c3be8ac3a6a5657a3473fa5c8e296
Will Becker <william.becker@bluefoxdata.eu>
2022-03-31 16:35:15 +0200

    rename and redo PCA

--- R/PCA.R
+++ R/PCA.R
@@ -1,4 +1,4 @@
-#' Perform PCA on a COIN
+#' Perform PCA on a coin
 #'
 #' Performs Principle Component Analysis (PCA) on a specified data set and subset of indicators or aggregation groups. Returns weights
 #' corresponding to the first principal component, i.e the weights that maximise the variance explained
@@ -7,37 +7,25 @@
 #' Note that `getPCA()` is simply a quick wrapper for [stats::prcomp()] which makes PCA on COINs quicker.
 #' See [COINr online documentation](https://bluefoxr.github.io/COINrDoc/multivariate-analysis.html#pca) for more details and examples.
 #'
-#' @param COIN An input object. The function can handle either the COIN object, or a data frame.
-#' The data frame should have each column as an indicator, and an optional column `"UnitCode"` which
-#' specifies the code (or name) of each unit. Any other type of object will return an error.
-#' @param dset If input is a COIN object, this specifies which data set in `.$Data` to use.
-#' @param icodes An optional character vector of indicator codes to subset the indicator data. Usefully, can also refer to
-#' an aggregation group name, and data will be sub-setted accordingly. NOTE does not work with multiple aggregate group names.
-#' @param aglev The aggregation level to take indicator data from. Integer from 1 (indicator level)
+#' @param coin A coin
+#' @param dset The name of the data set in `.$Data` to use.
+#' @param iCodes An optional character vector of indicator codes to subset the indicator data, passed to [get_data()]
+#' @param Level The aggregation level to take indicator data from. Integer from 1 (indicator level)
 #' to N (top aggregation level, typically the index).
 #' @param by_groups If `TRUE` (default), performs PCA inside each aggregation group inside the specified level. If `FALSE`,
 #' performs a single PCA over all indicators/aggregates in the specified level.
 #' @param nowarnings If `FALSE` (default), will give warnings where missing data are found. Set to `TRUE` to suppress these warnings.
-#' @param out2 If the input is a COIN object, this controls where to send the output. If `"COIN"`, it
-#' sends the results to the COIN object, otherwise if `"list"`, outputs to a separate list.
+#' @param out2 If the input is a coin object, this controls where to send the output. If `"coin"`, it
+#' sends the results to the coin object, otherwise if `"list"`, outputs to a separate list.
 #'
 #' @importFrom stats prcomp na.omit
-#' @importFrom rlang .data
 #'
 #' @examples
-#' # build ASEM COIN
-#' ASEM <- assemble(IndData = ASEMIndData, IndMeta = ASEMIndMeta,
-#' AggMeta = ASEMAggMeta)
-#' # get PCA results for pillar groups inside "Sust" (sustainability) sub-index
-#' # (warnings about missing data are suppressed)
-#' PCAres <- getPCA(ASEM, dset = "Raw", icodes = "Sust",
-#' aglev = 1, nowarnings = TRUE, out2 = "list")
-#' # summarise PCA results for Social pillar
-#' summary(PCAres$PCAresults$Social$PCAres)
+#' #
 #'
 #' @return
-#' If `out2 = "COIN"`, results are appended to the COIN object. Specifically:
-#' * A new set of PCA weights is added to .$Parameters$Weights
+#' If `out2 = "coin"`, results are appended to the coin object. Specifically:
+#' * A new set of PCA weights is added to `.$Meta$Weights`
 #' * A list is added to `.$Analysis` containing PCA weights (loadings) of the first principle component, and the output of [stats::prcomp], for each
 #' aggregation group found in the targeted level.
 #' If `out2 = "list"` the same outputs are contained in a list.
@@ -46,60 +34,63 @@
 #' * [stats::prcomp] Principle component analysis
 #'
 #' @export
+get_PCA <- function(coin, dset = "Raw", iCodes = NULL, Level = NULL, by_groups = TRUE,
+                   nowarnings = FALSE, out2 = "coin"){
 
-getPCA <- function(COIN, dset = "Raw", icodes = NULL, aglev = NULL, by_groups = TRUE,
-                   nowarnings = FALSE, out2 = "COIN"){
+  if(is.null(Level)){
+    Level <- 1
+  }
 
   # There is a catch here because we might want to do PCA weights across one level, but that level
   # may have multiple groups. This means we have to call PCA separately for each group.
 
   # first we define a function which returns weights for a given set of indicator data
-  # this function implicitly other variables from the environment inside getPCA() so we don't need
+  # this function implicitly calls other variables from the environment inside getPCA() so we don't need
   # to explicitly pass everything to it.
   PCAwts <- function(icodes1){
 
     # get ind data
-    out <- getIn(COIN, dset = dset, icodes = icodes1, aglev = aglev)
+    iData_ <- get_data(coin, dset = dset, iCodes = icodes1, Level = Level, also_get = "none")
 
     # check for missing vals
-    nNA <- sum(is.na(out$ind_data_only))
+    nNA <- sum(is.na(iData_))
 
     # remove any rows with missing data
     if (nNA > 0){
-      dat4PCA <- stats::na.omit(out$ind_data_only)
+      dat4PCA <- stats::na.omit(iData_)
       if(!nowarnings){
-        warning(paste0(nNA, " missing values found. Removing ", nrow(out$ind_data_only)-nrow(dat4PCA), " rows with missing values in order to perform
+        warning(paste0(nNA, " missing values found. Removing ", nrow(iData_)-nrow(dat4PCA), " rows with missing values in order to perform
 PCA. You can also try imputing data first to avoid this."))
       }
 
     } else {
-      dat4PCA <- out$ind_data_only
+      dat4PCA <- iData_
     }
 
     # perform PCA
     PCAres <- stats::prcomp(dat4PCA, center = TRUE, scale = TRUE)
 
-    # just for writing results - if aglev not specified then we are working at ind level
-    if(is.null(aglev)){aglev<-1}
+    # just for writing results - if Level not specified then we are working at ind level
+    if(is.null(Level)){Level<-1}
 
     # weight from first PC should be the max variance weights
-    wts <- PCAres$rotation[,1] %>% as.numeric()
+    wts <- as.numeric(PCAres$rotation[,1])
 
-    return(list(wts = wts, PCAres = PCAres, IndCodes = out$IndCodes))
+    list(wts = wts, PCAres = PCAres, IndCodes = names(iData_))
   }
 
   # We need to know the codes of the inds/aggs to get weights from
-  out3 <- getIn(COIN, dset = dset, icodes = icodes, aglev = aglev)
-  IndCodes <- out3$IndCodes
+  iData_full <- get_data(coin, dset = dset, iCodes = iCodes, Level = Level, also_get = "none")
+  IndCodes <- names(iData_full)
 
   if(by_groups){
     # OK, first thing is to find what groups we have
     # Get index structure
-    agcols <- dplyr::select(COIN$Input$IndMeta, .data$IndCode, dplyr::starts_with("Agg"))
+    lin <- coin$Meta$Lineage
     # Get cols of interest: the present one plus the parents
-    agcols <- agcols[c(aglev, aglev + 1)]
+    lin <- lin[c(Level, Level + 1)]
     # Get parents of these codes
-    parents <- unlist(unique(agcols[(agcols[[1]] %in% IndCodes) ,2]))
+    parents <- unlist(unique(lin[(lin[[1]] %in% IndCodes) ,2]))
   } else {
     parents = "All"
   }
@@ -109,7 +100,7 @@ PCA. You can also try imputing data first to avoid this."))
   # List for general PCA results
   PCAlist <- vector(mode = "list", length = length(parents))
   # copy of weights to modify
-  wlist <- COIN$Parameters$Weights$Original
+  wlist <- coin$Meta$Weights$Original
 
   for (ii in 1: length(parents)){
     if(by_groups){
@@ -122,7 +113,7 @@ PCA. You can also try imputing data first to avoid this."))
     # attach weights to list
     # wts should be in the same order as out$IndCodes. We have to make sure they match exactly here as
     # sometimes things get reordered. This is done with match() rather than %in% for this reason.
-    wlist$Weight[match(outPCA$IndCodes, wlist$Code)] <- outPCA$wts
+    wlist$Weight[match(outPCA$IndCodes, wlist$iCode)] <- outPCA$wts
     # add general results to list
     PCAlist[[ii]] <- outPCA
   }
@@ -130,14 +121,22 @@ PCA. You can also try imputing data first to avoid this."))
   names(PCAlist) <- parents
 
   # write results
-  if( (out3$otype == "COINobj") & (out2 == "COIN")){
-    eval(parse(text=paste0("COIN$Parameters$Weights$PCA_",dset,"L",aglev,"<-wlist")))
-    eval(parse(text=paste0("COIN$Analysis$",dset,"$PCA$L",aglev,"<- PCAlist")))
-    return(COIN)
+  if(out2 == "coin"){
+
+    if(is.null(w_name)){
+      w_name <- paste0("PCA_",dset,"L",Level)
+    }
+    # write weights
+    coin$Meta$Weights[[w_name]] <- wlist
+    # write other info
+    coin$Analysis[[dset]][[paste0("$PCA$L",Level)]] <- PCAlist
+
+    coin
+
   } else {
-    output <- list("Weights" = wlist,
+
+    list("Weights" = wlist,
                    "PCAresults" = PCAlist)
-    return(output)
   }
 
 }
