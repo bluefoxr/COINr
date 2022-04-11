e7490f264d4e9e7e6c3e3ca372b3983245ff7e8c
Will Becker <william.becker@bluefoxdata.eu>
2022-04-11 15:22:42 +0200

    Finish bar plot for now

--- R/plot_bar.R
+++ R/plot_bar.R
@@ -1,7 +1,8 @@
 
 #' Bar chart
 #'
-#' Plot bar charts of single indicators, possibly coloured according to a grouping variable.
+#' Plot bar charts of single indicators. Bar charts can be coloured by an optional grouping variable `by_group`, or if
+#' `iCode` points to an aggregate, setting `stack_children = TRUE` will plot `iCode` coloured by its underlying scores.
 #'
 #' @param coin A coin object.
 #' @param dset Data set from which to extract the variable to plot. Passed to [get_data()].
@@ -9,9 +10,13 @@
 #' @param ... Further arguments to pass to [get_data()], e.g. for filtering units.
 #' @param uLabel How to label units: either `"iCode"`, or `"iName"`.
 #' @param axes_label How to label the y axis and group legend: either `"uCode"` or `"uName"`.
-#' @param by_group Optional group variable to use to colour bars.
+#' @param by_group Optional group variable to use to colour bars. Cannot be used if `stack_children = TRUE`.
 #' @param dset_label Logical: whether to include the data set in the y axis label.
 #' @param log_scale Logical: if `TRUE` uses a log scale for the y axis.
+#' @param stack_children Logical: if `TRUE` and `iCode` refers to an aggregate, will plot `iCode` with each bar split into
+#' its underlying component values (the underlying indicators/aggregates used to create `iCode`). To use this, you must
+#' have aggregated your data and `dset` must point to a data set where the underlying (child) scores of `iCode` are available.
+#' @param bar_colours Optional vector of colour codes for colouring bars.
 #'
 #' @return A ggplot2 plot object.
 #' @export
@@ -19,13 +24,18 @@
 #' @examples
 #' #
 plot_bar <- function(coin, dset, iCode, ..., uLabel = "uCode", axes_label = "iCode",
-                     by_group = NULL, dset_label = FALSE, log_scale = FALSE){
+                     by_group = NULL, dset_label = FALSE, log_scale = FALSE, stack_children = FALSE,
+                     bar_colours = NULL){
 
   # PREP --------------------------------------------------------------------
 
   stopifnot(is.character(dset),
             is.character(iCode),
-            axes_label %in% c("iCode", "iName"))
+            length(iCode) == 1,
+            axes_label %in% c("iCode", "iName"),
+            is.logical(dset_label),
+            is.logical(log_scale),
+            is.logical(stack_children))
 
   if(!is.null(uLabel)){
     stopifnot(is.character(uLabel),
@@ -34,7 +44,6 @@ plot_bar <- function(coin, dset, iCode, ..., uLabel = "uCode", axes_label = "iCo
       stop("uLabel must be either NULL, 'uCode', or 'uName'.")
     }
   }
-
 
   # GET DATA ----------------------------------------------------------------
 
@@ -46,31 +55,87 @@ plot_bar <- function(coin, dset, iCode, ..., uLabel = "uCode", axes_label = "iCo
 
   iData <- get_data(coin, dset = dset, iCodes = iCode, also_get = also_get, ...)
 
+  # uLABELS -----------------------------------------------------------------
+
   if(is.null(uLabel) || (uLabel == "uCode") ){
     iData$plbs <- iData$uCode
   } else {
     iData$plbs <- codes2names(coin, iData$uCode)
   }
 
+  # GET children -------------------------------------------------------------
+  # if stack_children = TRUE, we need to get iCode plus underlying codes
+
+  if(stack_children){
+
+    if(!is.null(by_group)){
+      stop("Cannot have stack_children = TRUE and plotting by group (by_group). Disable one of these two options.")
+    }
+
+    # get iMeta
+    iMeta <- coin$Meta$Ind
+    # get child codes
+    iCodes_ch <- iMeta$iCode[iMeta$Parent == iCode]
+    # remove NAs
+    iCodes_ch <- iCodes_ch[!is.na(iCodes_ch)]
+    # check
+    if(length(iCodes_ch) == 0){
+      stop("No child codes found for selected iCode: if stack_children = TRUE, you must select an iCode in Level 2
+           or above (it must be an aggregate).")
+    }
+
+    # get data
+    iData_ch <- get_data(coin, dset = dset, iCodes = iCodes_ch, also_get = also_get, ...)
+
+    # merge onto iData
+    iData <- merge(iData, iData_ch, by = "uCode")
+
+    # scale children to add up to parent score
+    iData$scale_fac <- iData[[iCode]]/rowSums(iData[iCodes_ch])
+    iData[iCodes_ch] <- sapply(iData[iCodes_ch], `*`, iData$scale_fac)
+
+    # make long for plotting, and rename some things
+    iData <- lengthen(iData, cols = iCodes_ch)
+    names(iData)[names(iData) == "name"] <- "Component"
+    names(iData)[names(iData) == iCode] <- paste0(iCode, "2")
+    names(iData)[names(iData) == "Value"] <- iCode
+
+  }
+
   # PLOT --------------------------------------------------------------------
 
   # setup: whether to plot by group or not
   if(!is.null(by_group)){
-    plt <- ggplot2::ggplot(iData, ggplot2::aes(x = reorder(.data[["uCode"]], -.data[[iCode]]),
+    plt <- ggplot2::ggplot(iData, ggplot2::aes(x = reorder(.data[["plbs"]], -.data[[iCode]]),
                                              y = .data[[iCode]],
                                              label = plbs,
                                              fill = .data[[by_group]]))
+  } else if(stack_children){
+    plt <- ggplot2::ggplot(iData, ggplot2::aes(x = reorder(.data[["plbs"]], -.data[[iCode]]),
+                                               y = .data[[iCode]],
+                                               label = plbs,
+                                               fill = .data[["Component"]]))
   } else {
-    plt <- ggplot2::ggplot(iData, ggplot2::aes(x = reorder(.data[["uCode"]], -.data[[iCode]]),
+    plt <- ggplot2::ggplot(iData, ggplot2::aes(x = reorder(.data[["plbs"]], -.data[[iCode]]),
                                              y = .data[[iCode]],
                                              label = plbs))
   }
 
-  # main plot
-  plt <-  plt +
-    ggplot2::geom_bar(stat = "identity") +
-    ggplot2::theme_minimal()
+  if(stack_children){
 
+    # main plot
+    plt <-  plt +
+      ggplot2::geom_bar(stat = "identity", position = "stack") +
+      ggplot2::theme_minimal()
+
+  } else {
+
+    # main plot
+    plt <-  plt +
+      ggplot2::geom_bar(stat = "identity") +
+      ggplot2::theme_minimal()
+
+  }
 
   # LABELS ------------------------------------------------------------------
 
@@ -97,6 +162,12 @@ plot_bar <- function(coin, dset, iCode, ..., uLabel = "uCode", axes_label = "iCo
     )
   }
 
+
+  # COLOURS -----------------------------------------------------------------
+
+  if(!is.null(bar_colours)){
+    plt <- plt + ggplot2::scale_fill_manual(values = bar_colours)
+  }
 
   # AXES --------------------------------------------------------------------
 
